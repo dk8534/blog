0a142423dde23114a1c497390924c751b28408c1
dk8534 <darshangupta18gba17@gmail.com>
2022-11-27 16:08:25 +0530

    reverse 2

--- finalblog/settings.py
+++ finalblog/settings.py
@@ -121,9 +121,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
-
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
